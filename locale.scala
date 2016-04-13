09e4fa2a9d3383b63c4c1c6cb5670db594fc0373
MatrixMike <matroxmike@icloud.com>
2016-04-13 18:52:09 +1000

    added important gears and locale etc

---
+++ locale.scala
@@ -0,0 +1,46 @@
+import java.util.{Date, Locale}
+import java.text.DateFormat
+import java.text.DateFormat._ 
+
+object DateFormatDemoSO {
+def main(args: Array[String]) {
+  //  int style = DateFormat.MEDIUM;
+    //Also try with style = DateFormat.FULL and DateFormat.SHORT
+val now = new Date
+ //   DateFormat df;
+//    df = DateFormat.getDateInstance(style, Locale.UK);
+var df = getDateInstance(LONG, Locale.UK)
+	println(df format now)
+	
+	df = getDateInstance(LONG, Locale.US)
+	println(df format now)	
+
+	df = getTimeInstance(LONG, Locale.US)
+	println(df format now)	
+	
+	df = getDateInstance(LONG, Locale.ITALY)
+	println(df format now)	
+	
+	df = getDateInstance(LONG, Locale.JAPAN)
+	println(df format now)
+	
+	df = getDateInstance(LONG, Locale.FRANCE)
+	println(df format now)
+	
+	df = getDateInstance(LONG, Locale.KOREA)
+	println(df format now)
+	
+	df = getDateInstance(LONG, Locale.CANADA)
+	println(df format now)
+	/*
+    df = DateFormat.getDateInstance(style, Locale.US);
+    System.out.println("USA: " + df.format(date));   
+    df = DateFormat.getDateInstance(style, Locale.FRANCE);
+    System.out.println("France: " + df.format(date));
+    df = DateFormat.getDateInstance(style, Locale.ITALY);
+    System.out.println("Italy: " + df.format(date));
+    df = DateFormat.getDateInstance(style, Locale.JAPAN);
+    System.out.println("Japan: " + df.format(date));
+    * */
+  }
+}
---
+++ nested3loops.scala
@@ -0,0 +1,52 @@
+// mike 10-3 2015 
+// calculate all the possible combinations of gear ratios from the FrontList and RearList 
+// create a list from these values
+// then sort the list
+// and print the list in order
+// Wed 11 Mar 2015 00:39:16 EST 
+// into geany 23.03.2015
+
+import scala.collection.mutable.ListBuffer
+
+
+object BikeGears{
+def main(args: Array[String]) {
+var a = 0;
+var something = List(2.0, 3.0, 4.0);
+var FrontList = List(22.0, 32.0, 42.0);  // make float so division is 
+										//	automatically organised
+var RearList = List(14, 16, 18, 20, 22, 24, 34);       //26, 23, 21, 19, 15, 13, 12);  // Steve Red bike 
+// var RatioList = Nil;
+// for loop execution with multiple filters
+ var ratios = new ListBuffer[Double]()
+
+
+
+for( a <- FrontList; b <- RearList ; c<- something){
+// if a != 3;
+
+// if a < 8 ;
+ // if a >=2){
+ // RatioList = RatioList .::: a/b
+	ratios += a/b 
+
+	println( "Gear ratio of F/R: "   + a, +b,
+	a/b*c) ;
+}
+
+println( "\n");
+
+	//println( RatioList );
+	//println( ratios );   // unsorted
+	val ratiosList = ratios.toList
+// println("spacer line");
+	for ( a <- ratiosList.sorted ) {
+	// keep as example printf ("ratios %4.2f "+ a);
+		printf("ratio %f \n", a);
+	}
+	
+	println("Steve's Green Bike");
+// printf( ratiosList.sorted(3) );  // want to show the ratio of smallest to largest available
+}  // end main
+}  // end object
+// }
