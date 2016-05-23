f7beb4296562a8546c1ad936b1ec7d4b90228ec9
MatrixMike <matroxmike@icloud.com>
2016-05-23 20:08:50 +1000

    new code to replace deprecated code - actually worked first time for MJH - slight surprise

--- readScreen.scala
+++ readScreen.scala
@@ -1,7 +1,8 @@
 object Test2 {
 def main(args: Array[String]) {
 print("Please enter your input : " )
-val line = Console.readLine
+//val line = Console.readLine     23.05.2016 20:05:53  deprecated 
+val line =  scala.io.StdIn.readLine()    //       Console.readLine   replacement code with no warnings given
 println("Thanks, you just typed: " + line)
 }
 }
